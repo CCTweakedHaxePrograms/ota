597ebd9d69edc8b649568e99451293a3b090e721
PXshadow <joeyprather8@gmail.com>
2021-06-25 22:58:59 -0700

    first

---
+++ Main.hx
@@ -0,0 +1,10 @@
+import cc.Shell;
+
+function main() {
+    var args = Sys.args();
+    if (args.length == 0)
+        throw "invalid num of arguments, needs a repository";
+    var rep = args.shift();
+    var url = "https://github.com/CCTweakedHaxePrograms/";
+    Shell.run("wget",url + rep);
+}
